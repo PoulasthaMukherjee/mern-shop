a485eb39e5f7366ef367ba48ed76ba5cc272980c
Poulastha Mukherjee <poulasthamukherjee01@gmail.com>
2024-10-20 19:30:49 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,32 +1,25 @@
-require('dotenv').config(); // To use environment variables from .env
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/products-routes");
+
 const shopProductsRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
+
 const commonFeatureRouter = require("./routes/common/feature-routes");
+
+mongoose
+  .connect("mongodb+srv://poulasthamukherjee01:poulasthamukherjee2024@cluster0.5gnlt.mongodb.net/")
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB using environment variables
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB connected"))
-  .catch((error) => console.log(error));
-
-// CORS setup to allow frontend URLs
-const allowedOrigins = [
-  process.env.FRONTEND_URL_LOCAL,
-  process.env.FRONTEND_URL_PROD,
-];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5175"];
 
 app.use(
   cors({
@@ -38,20 +31,26 @@ app.use(
       }
     },
     methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
     credentials: true,
   })
 );
 
 app.use(cookieParser());
 app.use(express.json());
-
-// API Routes
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
+
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/search", shopSearchRouter);
+
 app.use("/api/common/feature", commonFeatureRouter);
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
